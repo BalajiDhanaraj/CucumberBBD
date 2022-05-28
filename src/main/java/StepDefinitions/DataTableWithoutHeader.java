571b79a9c1d637e1a80fbb708bc217080a899f40
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-05-28 15:47:11 +0530

    updated

--- src/main/java/StepDefinitions/DataTableWithoutHeader.java
+++ src/main/java/StepDefinitions/DataTableWithoutHeader.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -21,7 +22,7 @@ public class DataTableWithoutHeader {
         driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
     }
     @When("you should enter the login credentials")
-    public void you_should_enter_the_login_credentials(io.cucumber.datatable.DataTable dataTable) {
+    public void you_should_enter_the_login_credentials(DataTable table) {
         // Write code here that turns the phrase above into concrete actions
         // For automatic transformation, change DataTable to one of
         // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
@@ -29,10 +30,14 @@ public class DataTableWithoutHeader {
         // Double, Byte, Short, Long, BigInteger or BigDecimal.
         //
         // For other transformations you can register a DataTableType.
-        List<String> credentials = dataTable.asList(String.class); // this is used to retreive the data from feature file
 
-        driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys(credentials.get(0));
-        driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(credentials.get(1));
+        List<String> credentials = table.asList(String.class);
+        // this is used to retreive the data from feature file
+
+        String name = credentials.get(0);
+        String pass = credentials.get(1);
+        driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys(name);
+        driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys(pass);
     }
     @When("you should click the login button")
     public void you_should_click_the_login_button() {
