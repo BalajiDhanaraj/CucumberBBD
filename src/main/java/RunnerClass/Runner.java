7814243dc65ee2989fcbc7eb47082aab78143b7f
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-06-04 12:00:39 +0530

    updated

--- src/main/java/RunnerClass/Runner.java
+++ src/main/java/RunnerClass/Runner.java
@@ -9,10 +9,13 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "/Volumes/Macintosh HD/For Mac/java text and soft file and java IDE/Intellij java project/CucumberBBD/src/main/java/FeatureFile/Login.feature",
         glue = "StepDefinitions",
         stepNotifications = true,
-        dryRun = true
+        dryRun = true,
+        monochrome = true
         )
 public class Runner {
 
 }
    // dry run will not execute, but it will check the feature file and stepdefinition are correctly mapped with eachother, it will validate and verification, self evalution
-   // strict is deprecated in cucumber 6, strict will execute and check if both the feature and stepdefinition
+   // strict is deprecated in cucumber 6, strict will execute and check if both the feature and step definition
+   // dryrun and strict are support boolean
+   // monochrome to print human readable in console
